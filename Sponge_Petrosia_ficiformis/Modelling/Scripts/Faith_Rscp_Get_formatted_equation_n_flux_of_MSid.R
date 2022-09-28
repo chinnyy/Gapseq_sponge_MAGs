7129354169d374bb2e4e6f2e8b64e56877248724
chinnyy <chinyeesim33@gmail.com>
2022-09-28 15:17:18 +1000

    28/9/22 Sponge_Petrosia_ficiformis update

--- Sponge_Petrosia_ficiformis/Modelling/Scripts/Faith_Rscp_Get_formatted_equation_n_flux_of_MSid.R
+++ Sponge_Petrosia_ficiformis/Modelling/Scripts/Faith_Rscp_Get_formatted_equation_n_flux_of_MSid.R
@@ -3,17 +3,20 @@
 #### IMPORTANT: ModelSEED reaction id must be in the 1st column with header start with 'react_id'.
 
 # Set to where you want the script to run on 
-dir_out = setwd('/Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts')
+dir_out = setwd("C:/Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts")
 
 gapseq_version  = 'gapseq_v20220329'
 
 # Import the gap-filled modified object model (gfM): 
 # Change accordingly to the name of the object model!
 gfM_dir = 'Actino_5_Burgsdorf_2021_full/'
+
 model_id = 'Actino_5_Burgsdorf_2021_full' 
 
+bac_id ='Actino_5_Burgsdorf_2021'
+
 # Navigate to the python code "Get_formatted_equations_n_flux_of_MS_react_in_gfModel.py"
-python_code  = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts/Python_scripts/Get_formatted_equations_n_flux_of_MS_react_in_gfModel.py'
+python_code  = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Cymbastela_concentrica/Modelling/Scripts/Python_scripts/Get_formatted_equations_n_flux_of_MS_react_in_gfModel.py'
 
 # Navigate to where the seed reaction tsv "seed_reactions_corrected.formatted_reat_add.tsv"
 dict_seed_reactions_corrected_tsv   = paste('/Users/chiny/Bioinfo/software/gapseq/',gapseq_version,'/Shan_output_files/seed_reactions_corrected.formatted_reat_add.tsv',sep = '')
@@ -24,7 +27,7 @@ infile_flux = paste(dir_out,'/07_gf_model/',gfM_dir,'050_model_',model_id,'_gfM_
 # Navigate to file with flux of MS ids (050_model_xxxxxx_gfMOM_Smat_fluxes.csv)
 infile_key_list = paste(dir_out,'/07_gf_model/',gfM_dir,model_id,'_completely_filled_in_reactions.txt',sep = '')
 
-outfile_key_list_equation_formatted = paste(dir_out,'/medium',model_id,'_completely_filled_in_reactions.equation_formatted_n_fluxes.txt',sep = '')
+outfile_key_list_equation_formatted = paste(dir_out,'/07_gf_model/',gfM_dir,model_id,'_completely_filled_in_reactions.equation_formatted_n_fluxes.txt',sep = '')
 
 commands = paste(python_code,
                  '-infile_MSid',infile_key_list,
--- Sponge_Petrosia_ficiformis/Modelling/Scripts/Faith_Rscp_check_flux_in_gfMOM.R
+++ Sponge_Petrosia_ficiformis/Modelling/Scripts/Faith_Rscp_check_flux_in_gfMOM.R
@@ -9,16 +9,18 @@ library(dplyr)
 # Set working directory to where the Bacteria folder is located
 setwd= setwd("C:/Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts")
 
+
 # Import the gap-filled modified object model (gfM): 
+gfM_dir = 'Actino_5_Burgsdorf_2021_liter_2/'
 
-gfM_dir = 'Actino_5_Burgsdorf_2021_liter_1/'
+model_id = 'Actino_5_Burgsdorf_2021_liter_2' 
 
-model_id = 'Actino_5_Burgsdorf_2021_liter_1'
+bac_id ='Actino_5_Burgsdorf_2021'
 
 # Create new folder where the output will be stored
 new_folder_nm = paste('/07_gf_model/',gfM_dir,sep = '')
 dir.create("07_gf_model")
-dir.create(paste(setwd,new_folder_nm,sep = ''))
+dir.create(paste(setwd,'/07_gf_model/',gfM_dir,sep = ''))
 
 
 # Assign output RDS file name:
@@ -31,9 +33,11 @@ outfile_050_2  = paste(setwd, new_folder_nm,'/050_2_model_',model_id,'_gfM_Smat_
 outfile_050_3 = paste(setwd, new_folder_nm,'/050_model_',model_id,'_gfM_Smat_fluxes_ETC_complexes.tsv',sep = "")
 
 # Load the model RDS file
-model_gfm <- readRDS(paste(setwd,"/Input_files/Actino_5_Burgsdorf_2021_liter_1/Actino_5_Burgsdorf_2021_liter_1.RDS",sep = ""))
+model_gfm <- readRDS(paste(setwd,"/Input_files/",gfM_dir,model_id,".RDS",sep = ""))
 
-## Warning: In readRDS(paste(setwd, "/Input_files/Actino_5_Burgsdorf_2021_liter_1/Actino_5_Burgsdorf_2021_liter_1.RDS", sep = "")) : strings not representable in native encoding will be translated to UTF-8
+
+
+## Warning: In readRDS(paste(setwd, "/Input_files/bin.2/bin.2.RDS", sep = "")) : strings not representable in native encoding will be translated to UTF-8
 
 #### STEP 1.2: EXTRACT KEGG REACTION IDS FROM .RDS FILE ####
 
@@ -108,9 +112,8 @@ write.csv(cbind_t_model_gfm_Smat, quote = FALSE,file = outfile_050)
 ######################## Follow with python script of 'Assign_names_2_ID_of_active_rxn_in_Smax_arg.py'######################## 
 
 gapseq_version = 'gapseq_v20220329'
-input_dir = paste(setwd,'/07_gf_model/',gfM_dir,sep = '')
-model_id = 'Actino_5_Burgsdorf_2021_liter_1'
-mycmd1 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts/Python_scripts/Assign_names_2_ID_of_active_rxn_in_Smax_arg.py'
+input_dir = paste(setwd,'/07_gf_model/',model_id,'/',sep = '')
+mycmd1 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Cymbastela_concentrica/Modelling/Scripts/Python_scripts/Assign_names_2_ID_of_active_rxn_in_Smax_arg.py'
 
 # Please open "Assign_names_2_ID_of_active_rxn_in_Smax_arg.py" and modify the full path of Dict input files from line 78 to line 80.  
 
@@ -121,18 +124,18 @@ system(paste(mycmd1,
 
 
 # output files:
-# 051_model_Actino_5_Burgsdorf_2021_liter_1_gfM_Smat_fluxes.flux_lt0.csv
-# 052_model_Actino_5_Burgsdorf_2021_liter_1_gfM_Smat_fluxes.flux_lt0.annotate.tsv
+# 051_model_bin.2_gfM_Smat_fluxes.flux_lt0.csv
+# 052_model_bin.2_gfM_Smat_fluxes.flux_lt0.annotate.tsv
 
 
 ############################################ Run the following script once to get the "seed_reactions_corrected.formatted_reat_add.status.tsv" for the related 043 output file:####################################################
 
-mycmd2 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts/Python_scripts/Get_a_file_of_formatted_MS_reactions_with_assigned_reaction_and_pathway_status.py'
+mycmd2 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Cymbastela_concentrica/Modelling/Scripts/Python_scripts/Get_a_file_of_formatted_MS_reactions_with_assigned_reaction_and_pathway_status.py'
 
 # Open the python script "Get_a_file_of_formatted_MS_reactions_with_assigned_reaction_and_pathway_status.py", and change the path of "dict_gs_seed_rxn" in line 63.
 
 # path to all-Pathways.tbl
-all_path = paste(setwd,'/Input_files/',gfM_dir,'Actino_5_Burgsdorf_2021-all-Pathways.tbl', sep = '')
+all_path = paste(setwd,'/Input_files/',gfM_dir,bac_id,'-all-Pathways.tbl', sep = '')
 model_043 = paste('043_model_',model_id,'_gfM_list_react.tsv', sep = '')
 
 system(paste(mycmd2,
@@ -210,7 +213,7 @@ list_met = list('cpd00013', #NH3-c0	and NH3-e0
 )
 
 
-mycmd3 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts/Python_scripts/Get_active_rxn_Equ_annot_by_met_from_final_model_argv3.py'
+mycmd3 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Cymbastela_concentrica/Modelling/Scripts/Python_scripts/Get_active_rxn_Equ_annot_by_met_from_final_model_argv3.py'
 
 # Create a new file to store 053_model
 dir.create(paste(setwd,'/07_gf_model/',model_id,'/053_model',sep = ''), showWarnings = TRUE, recursive = TRUE)
@@ -234,7 +237,7 @@ for (met in list_met) {
 ######################## Follow with python script of 'Get_ETC_complexes_reaction_flux_argv.py '######################## 
 
 
-mycmd4 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Petrosia_ficiformis/Modelling/Scripts/Python_scripts/Get_ETC_complexes_reaction_flux_argv.py'
+mycmd4 = 'py /Users/chiny/Desktop/UNSW/Gapseq_sponge_MAGs/Sponge_Cymbastela_concentrica/Modelling/Scripts/Python_scripts/Get_ETC_complexes_reaction_flux_argv.py'
 
 system(paste(mycmd4,
              '-in_dict ',outfile_050,
@@ -245,7 +248,7 @@ system(paste(mycmd4,
 
 
 # output files:
-# 050_model_Actino_5_Burgsdorf_2021_liter_1_gfM_Smat_fluxes_ETC_complexes.tsv
+# 050_model_bin.2_gfM_Smat_fluxes_ETC_complexes.tsv
 
 ### New input by faith,
 ### This additional part is to generate the "XXXX_completely_filled_in_reactions.txt"
@@ -254,27 +257,26 @@ system(paste(mycmd4,
 
 ## Generating files based on draft model
 
-draft_id <- "draft_Actino_5_Burgsdorf_2021_liter_1"
 
 # Create a new file to store 053_model
-dir.create(paste(setwd,'/07_gf_model/',model_id,'/draft',sep = ''), showWarnings = TRUE, recursive = TRUE)
+dir.create(paste(setwd,'/07_gf_model/','draft',sep = ''), showWarnings = TRUE, recursive = TRUE)
 
-draft_folder<- paste(new_folder_nm,'draft/',sep = '')
+draft_folder<- paste('/07_gf_model/','draft',sep = '')
 
 
 # Assign output RDS file name:
-outfile_add_043= paste(setwd, draft_folder,'/043_model_',draft_id,'_gfM_list_react.tsv',sep = '')
-outfile_add_045= paste(setwd, draft_folder,'/045_model_',draft_id,'_gfM_list_metabolites.tsv',sep = '')
-outfile_add_046= paste(setwd, draft_folder,'/046_model_',draft_id,'_gfM_Smat.csv',sep = '')
+outfile_add_043= paste(setwd, draft_folder,'/043_model_draft_',bac_id,'_gfM_list_react.tsv',sep = '')
+outfile_add_045= paste(setwd, draft_folder,'/045_model_draft_',bac_id,'_gfM_list_metabolites.tsv',sep = '')
+outfile_add_046= paste(setwd, draft_folder,'/046_model_draft_',bac_id,'_gfM_Smat.csv',sep = '')
 infile_046     = outfile_add_046
-outfile_050    = paste(setwd, draft_folder,'/050_model_',draft_id,'_gfM_Smat_fluxes.csv',sep = "")
-outfile_050_2  = paste(setwd, draft_folder,'/050_2_model_',draft_id,'_gfM_Smat_fluxes.csv',sep = "")
-outfile_050_3 = paste(setwd, draft_folder,'/050_model_',draft_id,'_gfM_Smat_fluxes_ETC_complexes.tsv',sep = "")
+outfile_050    = paste(setwd, draft_folder,'/050_model_draft_',bac_id,'_gfM_Smat_fluxes.csv',sep = "")
+outfile_050_2  = paste(setwd, draft_folder,'/050_2_model_draft_',bac_id,'_gfM_Smat_fluxes.csv',sep = "")
+outfile_050_3 = paste(setwd, draft_folder,'/050_model_draft_',bac_id,'_gfM_Smat_fluxes_ETC_complexes.tsv',sep = "")
 
 # Load the model RDS file
-draft_gfm <- readRDS(paste(setwd,"/Input_files/Actino_5_Burgsdorf_2021_liter_1/Actino_5_Burgsdorf_2021-draft.RDS",sep = ""))
+draft_gfm <- readRDS(paste(setwd,"/Input_files/",gfM_dir,bac_id,"-draft.RDS",sep = ""))
 
-## Warning message: In readRDS(paste(setwd, "/Input_files/Actino_5_Burgsdorf_2021_liter_1/Actino_5_Burgsdorf_2021_liter_1-draft.RDS", sep = "")) :strings not representable in native encoding will be translated to UTF-8
+## Warning message: In readRDS(paste(setwd, "/Input_files/bin.2/bin.2-draft.RDS", sep = "")) :strings not representable in native encoding will be translated to UTF-8
 
 #### STEP 1.2: EXTRACT KEGG REACTION IDS FROM .RDS FILE ####
 
@@ -353,7 +355,7 @@ write.csv(cbind_t_draft_gfm_Smat, quote = FALSE,file = outfile_050)
 
 filled_rxn<-anti_join(df_model_gfm_list,df_draft_gfm_list, by="react_id")
 
-write.table(filled_rxn, file = paste(setwd,new_folder_nm,model_id,"_completely_filled_in_reactions.txt", sep = ""),quote = F,row.names = F,sep='\t')
+write.table(filled_rxn, file = paste(setwd,new_folder_nm,'/',model_id,"_completely_filled_in_reactions.txt", sep = ""),quote = F,row.names = F,sep='\t')
 
 
 
